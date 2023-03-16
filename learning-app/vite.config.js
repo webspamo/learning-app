bd478def3b51f433e91a01764072a561f43da4ed
Yaroslav <webspamo@protonmail.com>
2023-03-16 22:58:05 +0200

    add proxy configuration resolving CORS policy

--- learning-app/vite.config.js
+++ learning-app/vite.config.js
@@ -1,14 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://api.wisey.app",
+                changeOrigin: true,
+                //rewrite: (path) => path.replace(/^\/api/, ''),
+                //rewrite option is used to remove the /api prefix from the request path, so that it matches the API server's expected route.
+            },
+        },
+    },
+});
