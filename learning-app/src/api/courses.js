c25b66db551102bca3cde63935ddcfb3e00aa6a4
Yaroslav <webspamo@protonmail.com>
2023-03-18 21:08:03 +0200

    request courses list from postman using axios

---
+++ learning-app/src/api/courses.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkOTRlNjg4NS1kM2U5LTQwY2EtYTVjYy01MDRkNjZlZDVlN2QiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg3MDQ3NjIsImV4cCI6MTY3OTYwNDc2Mn0.Qw3LF39CDp27ZxoGzt5rikJM_OTx0eNaoyFFLxxrXUM";
+axios.defaults.headers.Authorization = `Bearer ${token}`;
+axios.defaults.baseURL = `http://localhost:5173/api/v1`;
+
+export async function getCourses() {
+    return await axios.get("/core/preview-courses");
+}
