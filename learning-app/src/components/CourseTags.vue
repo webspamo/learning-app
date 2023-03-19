7c2d0c2f7d62e63681b84e5d483908db9dfb34a3
Yaroslav <webspamo@protonmail.com>
2023-03-19 19:22:20 +0200

    rebuild CourseTags.vue as a component

---
+++ learning-app/src/components/CourseTags.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul class="tags">
+        <li
+            class="tag"
+            v-for="(tag, index) in tags"
+            :key="index">
+            {{ tag }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "CourseTags",
+    components: {},
+    props: {
+        tags: Array,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/_mixins";
+
+.tags {
+    list-style: none;
+    .tag {
+        padding: 0.1rem 0.35rem;
+        font-family: "Josefin Sans", sans-serif;
+        font-weight: 500;
+        border-radius: 5px;
+        background: rgba(255, 255, 255, 0.2);
+    }
+}
+</style>
